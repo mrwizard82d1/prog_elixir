b191c78fb1b4f804a456beaa071c92c5fa563c4f
Larry Jones <larry.jones@reveal-energy.com>
2021-04-05 15:17:01 -0500

    Tweak application using application configuration

---
+++ issues/config/config.exs
@@ -0,0 +1,7 @@
+defmodule Config do
+  @moduledoc false
+
+  import Config
+
+  config :issues, github_url: "https://api.github.com"
+end
--- issues/lib/issues/issues/github_issues.ex
+++ issues/lib/issues/issues/github_issues.ex
@@ -4,18 +4,17 @@ defmodule Issues.GithubIssues do
   def fetch(user, project) do
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
-    # |> HTTPoison.get()
     |> handle_response
   end
 
+  # Use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     {:ok, :jsx.decode(body)}
   end
-  def handle_response({:error, reason}) do
-    {:error, :jsx.decode(reason)}
-  end
+  def handle_response({:error, reason}), do: {:error, reason}
 end
