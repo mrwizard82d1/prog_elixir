267f4cdc9157f2e82b6d2f02409eef87808ce6e8
Larry Jones <larry.jones@reveal-energy.com>
2021-04-08 13:09:56 -0500

    Test "output is correct"

--- issues/lib/issues/issues/table_formatter.ex
+++ issues/lib/issues/issues/table_formatter.ex
@@ -1,4 +1,14 @@
 defmodule Issues.TableFormatter do
+  def print_table_for_columns(rows, headers) do
+    data_by_columns = split_into_columns(rows, headers)
+    column_widths = widths_of(data_by_columns)
+    format = format_for(column_widths)
+
+    puts_one_line_in_columns headers, format
+    IO.puts separator(column_widths)
+    puts_in_columns data_by_columns, format
+  end
+
   def split_into_columns(rows, headers) do
     for header <- headers do
       for row <- rows, do: printable(row[header])
@@ -16,4 +26,18 @@ defmodule Issues.TableFormatter do
     Enum.map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
   end
 
+  def separator(column_widths) do
+    Enum.map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
+  end
+
+  def puts_in_columns(data_by_columns, format) do
+    data_by_columns
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.each(&puts_one_line_in_columns(&1, format))
+  end
+
+  def puts_one_line_in_columns(fields, format) do
+    :io.format(format, fields)
+  end
 end
--- issues/test/table_formatter_test.exs
+++ issues/test/table_formatter_test.exs
@@ -32,4 +32,24 @@ defmodule TableFormatterTest do
     assert Itf.format_for([9, 10, 11]) == "~-9s | ~-10s | ~-11s~n"
   end
 
+  test "output is correct" do
+    result = capture_io fn() -> Itf.print_table_for_columns(simple_test_data(), headers()) end
+    # The book implementation uses a `heredoc` to construct the expected result. The system under test pads
+    # results with leading **and** trailing whitespace. My editor, however, truncates trailing whitespace
+    # from lines. Consequently, the test fails, so I actually use `Enum.join` to join a `List` of each
+    # expected line. Additionally, the last line of the actual contains a trailing newline. I add this
+    # character by appending an "extra" empty string.
+    expected_lines = [
+      "c1    | c2     | c4     ",
+      "------+--------+--------",
+      "r1 c1 | r1 c2  | r1+++c4",
+      "r2 c1 | r2 c2  | r2 c4  ",
+      "r3 c1 | r3 c2  | r3 c4  ",
+      "r4 c1 | r4++c2 | r4 c4  ",
+      "",
+    ]
+    expected = Enum.join(expected_lines, "\n")
+    assert result == expected
+  end
+
 end
