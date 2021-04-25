e391fc0dc02b82434158a919ed7dc4a0a1912717
Larry Jones <larry.jones@reveal-energy.com>
2021-04-24 20:07:55 -0500

    Two processes: trap exits but not linked

---
+++ spawn/lib/link1.ex
@@ -0,0 +1,17 @@
+defmodule Link1 do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep 500
+    exit(:boom)
+  end
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn(Link1, :sad_function, [])
+    receive do
+      msg -> IO.puts "MESSAGE RECEIVED: #{inspect(msg)}"
+    after 1000 -> IO.puts "Nothing happened as far as I know"
+    end
+  end
+end
